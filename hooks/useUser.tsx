8f45144b4ced2dc30254dc8029e45eb52404d4d0
Athesh007 <kavipprasath80@gmail.com>
2023-10-15 18:58:37 +0530

    auth-modal

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,10 +1,10 @@
 import { Subscription, userDetails } from "@/types";
-import { User } from "@supabase/auth-helpers-nextjs";
 import {
   useSessionContext,
   useUser as useSupaUser,
+  User,
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -75,4 +75,13 @@ export const MyUserContextProvider = (props: Props) => {
     subscription,
   };
   return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a MyUserContextProvider");
+  }
+
+  return context;
 };
